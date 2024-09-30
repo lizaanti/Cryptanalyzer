2c012aebc66b12b8d68abffa006f6a7da80f6221
lizaanti <liza.anti2005@gmail.com>
2024-09-30 17:34:43 +0300

    encryption and decoder

--- src/main/java/EncryptionAlgorithm.java
+++ src/main/java/EncryptionAlgorithm.java
@@ -2,18 +2,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class EncryptionAlgorithm {
+    private static String inputStr;
+    private static int rotn;
+    private static char[] charStr;
     static void encryption(){
-        String inputStr;
-        char temp;
-        int rotn;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку: ");
         inputStr = scanner.nextLine();
         inputStr = inputStr.toLowerCase();
-        StringBuilder res = new StringBuilder(inputStr.length());
-        char[] charStr = inputStr.toCharArray();
         System.out.println("Введите количество символов для сдвига: ");
         rotn = scanner.nextInt();
+        StringBuilder res;
+        res = new StringBuilder(inputStr.length());
+        char temp;
+        charStr = inputStr.toCharArray();
         char[] russianAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
         char[] americanAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         for (int i = 0; i < charStr.length; i++) {
@@ -34,6 +36,37 @@ public class EncryptionAlgorithm {
                         temp = (char) (temp % 'z' + 'a' - 1);
                     } else if (temp < 'a') {
                         temp = (char) (temp + (('z' - 'a') + 1));
+                    }
+                }
+            }
+            res.append(temp);
+        }
+        System.out.println(res);
+    }
+
+    static void decoder(){
+        char[] russianAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+        char[] americanAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char temp;
+        StringBuilder res = new StringBuilder(inputStr.length());
+        for (int i = 0; i < charStr.length; i++) {
+            temp = charStr[i];
+            if(Character.isLetter(charStr[i])){
+                if (Arrays.binarySearch(russianAlphabet, temp) >= 0) { //Метод binarySearch() в используется для поиска элемента в отсортированном массиве
+                    temp -= (char) (rotn % russianAlphabet.length);
+                    if(temp < 'а' ){
+                        temp = (char)(temp + (('я' + 'а') + 1));
+                    }
+                    else if (temp > 'я') {
+                        temp = (char) (temp % 'я' + 'а' - 1);
+                    }
+                }
+                else if (Arrays.binarySearch(americanAlphabet, temp) >= 0) {
+                    temp -= (char) (rotn % americanAlphabet.length);
+                    if (temp < 'a') {
+                        temp = (char) (temp + (('z' - 'a') + 1));
+                    } else if (temp > 'z') {
+                        temp = (char) (temp % 'z' + 'a' - 1);
                     }
                 }
             }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
         EncryptionAlgorithm.encryption();
+        EncryptionAlgorithm.decoder();
     }
 }
